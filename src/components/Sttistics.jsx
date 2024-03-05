1252927ac5e36d61f3aafe5bf900c695006e6d31
Narges Ghanbari <narges.ghanbari@gmail.com>
2024-03-05 10:25:38 +0200

    Do the unicafe step 4

--- src/components/Sttistics.jsx
+++ src/components/Sttistics.jsx
@@ -5,17 +5,17 @@ const Statistics = (props) => {
   if (total !== 0) {
     average = (props.good - props.bad) / total;
     positive = (props.good / total) * 100;
-  }
-  return (
-    <div>
-      <h3>good {props.good}</h3>
-      <h3>neutral {props.neutral}</h3>
-      <h3>bad {props.bad}</h3>
-      <h3>all {total}</h3>
-      <h3>average {average}</h3>
-      <h3>positive {positive} %</h3>
-    </div>
-  );
+    return (
+      <div>
+        <h3>good {props.good}</h3>
+        <h3>neutral {props.neutral}</h3>
+        <h3>bad {props.bad}</h3>
+        <h3>all {total}</h3>
+        <h3>average {average}</h3>
+        <h3>positive {positive} %</h3>
+      </div>
+    );
+  } else return <h3>No feedback given</h3>;
 };
 
 export default Statistics;
