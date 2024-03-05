488c1168d675ba199a6d1dbd7af047784d06bcb9
Narges Ghanbari <narges.ghanbari@gmail.com>
2024-03-05 12:07:56 +0200

    Fix any waring in consol by tboy tag

--- src/components/Sttistics.jsx
+++ src/components/Sttistics.jsx
@@ -5,19 +5,21 @@ const Statistics = (props) => {
   if (total !== 0) {
     return (
       <table>
-        <StatisticLine text="good" value={props.good} />
-        <StatisticLine text="neutral" value={props.neutral} />
-        <StatisticLine text="bad" value={props.bad} />
-        <StatisticLine text="all" value={total} />
-        <StatisticLine
-          text="average"
-          value={(props.good - props.bad) / total}
-        />
-        <StatisticLine
-          text="positive"
-          value={(props.good / total) * 100}
-          text2="%"
-        />
+        <tbody>
+          <StatisticLine text="good" value={props.good} />
+          <StatisticLine text="neutral" value={props.neutral} />
+          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine
+            text="average"
+            value={(props.good - props.bad) / total}
+          />
+          <StatisticLine
+            text="positive"
+            value={(props.good / total) * 100}
+            text2="%"
+          />
+        </tbody>
       </table>
     );
   } else return <h3>No feedback given</h3>;
